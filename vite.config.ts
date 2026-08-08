35b66b91f60ae900e17cfd138f96bbc63737746d
Suhail Malik <iamdatascientistcg@gmail.com>
2026-08-08 19:13:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-// @lovable.dev/vite-tanstack-config already includes
-// TanStack Start, React, Tailwind, Nitro, path aliases, etc.
-
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
     server: {
       entry: "server",
+    },
+
+    spa: {
+      enabled: true,
     },
   },
 });
